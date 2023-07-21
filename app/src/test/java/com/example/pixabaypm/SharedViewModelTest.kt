5a436c99d1deb5438a6c72c09a83672cc841b562
Piotr Malak <malak.pete@gmail.com>
2023-07-21 19:20:36 +0200

    dependency simplified init block added to the view model

--- app/src/test/java/com/example/pixabaypm/SharedViewModelTest.kt
+++ app/src/test/java/com/example/pixabaypm/SharedViewModelTest.kt
@@ -1,27 +1,32 @@
 package com.example.pixabaypm
 
 import androidx.lifecycle.SavedStateHandle
+import com.example.pixabaypm.domain.usecase.GetPicturesUseCase
 import com.example.pixabaypm.ui.SharedViewModel
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
 
 //Test class for SharedViewModel
 class SharedViewModelTest {
 
     private lateinit var viewModel: SharedViewModel
 
+    private val mockGetPicturesUseCase = mock(GetPicturesUseCase::class.java)
+
     @Before
     fun setup() {
-        val savedState = SavedStateHandle(mapOf("query" to testId))
-        viewModel = SharedViewModel(savedStateHandle = savedState)
+        val savedState = SavedStateHandle(mapOf("query" to testQuery))
+        viewModel = SharedViewModel(savedStateHandle = savedState, mockGetPicturesUseCase)
     }
 
-    //test method onQueryChanged() of SharedViewModel
-    fun onQueryChanged_Test() {
-
+    @Test
+    fun `check if query is changed in state flow`()  = runTest{
 
     }
 
     companion object {
-        const val testId = "testId"
+        const val testQuery = "dogs"
     }
 }
